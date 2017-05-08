0c2b5a78890be997f1b699032b59a00c9bb6a9ed
EddyJo <jkjk0600@gmail.com>
2017-05-08 14:51:31 +0900

    LOGIN/LOGOUT API(using by session)

--- 03.express_tutorial/server.js
+++ 03.express_tutorial/server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 var server = app.listen(3000, function(){
-  console.log("Experss server has started on port 3000")
+  console.log("Experss server has started on port 3000");
 });
 
 app.use(express.static('public'));
